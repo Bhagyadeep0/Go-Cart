84bc173d230c0917b76a1be75ca85fa035e82bd6
Bhagyadeep <b.ghosh0330@gmail.com>
2024-04-09 21:21:41 +0530

    products added

---
+++ src/components/Products.jsx
@@ -0,0 +1,86 @@
+// react-icons
+import { AiFillShopping } from "react-icons/ai";
+
+const Products = () => {
+  //product data
+  const productsData = [
+    { name: "Classic something", price: "$39.99", img: "/pet-pink.jpg" },
+    { name: "Classic something", price: "$39.99", img: "/pet-pink.jpg" },
+    { name: "Classic something", price: "$39.99", img: "/pet-pink.jpg" },
+    { name: "Classic something", price: "$39.99", img: "/pet-pink.jpg" },
+  ];
+
+  return (
+    <section>
+      <div className="flex flex-col space-y-10 container mx-auto py-10">
+        {/* text & buttons */}
+        <div className=" flex flex-col lg:flex-row gap-5 justify-between items-center">
+          {/* text */}
+          <div className="max-w-[500px] flex-1">
+            <h2 className="h2 capitalize">Discover fashion</h2>
+            <p className="p">
+              Dive into a world of fashion innovation at Klothink. Our carefully
+              curated collections bring together the latest trends and timeless
+              classics, ensuring you find the perfect pieces for every occasion.
+            </p>
+          </div>
+          {/* buttons */}
+          <div className="flex-1">
+            <button className="btn bg-yellow-300 hover:bg-yellow-600 capitalize">
+              View all Products
+            </button>
+          </div>
+        </div>
+        {/* items */}
+        <div className="space-y-10">
+          {/* text, buttons */}
+          <div className="space-y-3 flex flex-col lg:flex-row items-center justify-between">
+            {/* text */}
+            <div className="flex-1 flex gap-5 uppercase">
+              <p>all</p>
+              <p>menswaear</p>
+              <p>womenswear</p>
+              <p>kidswear</p>
+            </div>
+            {/* buttons */}
+            <div className="space-x-5">
+              <button className="uppercase btn">casual</button>
+              <button className="uppercase btn">formal</button>
+            </div>
+          </div>
+          {/* img container */}
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 px-5 lg:px-0">
+            {/* data */}
+            {productsData.map((item, index) => (
+              <div
+                key={index}
+                className="flex flex-col flex-wrap cursor-pointer"
+              >
+                {/* img */}
+                <div className="w-fit overflow-hidden relative group rounded-xl">
+                  <img
+                    src={item.img}
+                    alt="product.img"
+                    className="object-cover group-hover:scale-110 transition-all duration-500"
+                  />
+                  <span className="absolute -bottom-5 -right-5 bg-white p-10 z-[1] rounded-full "></span>
+                  <span className="z-[2] absolute bottom-0 right-0 text-3xl bg-yellow-300 group-hover:bg-yellow-600 p-2 rounded-full transition-all duration-300">
+                    <AiFillShopping className="z-50" />
+                  </span>
+                </div>
+                {/* text */}
+                <div className="my-3">
+                  <p className="p">{item.name}</p>
+                  <p>{item.price}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Products;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@
 
 import Header from "../components/Header"
 import Hero from "../components/Hero"
+import Products from "../components/Products"
 
 const Home = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Products />
     </>
   )
 }
