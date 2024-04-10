3fa6bee7467398f4e5c0f532d5f90db173209ce1
Bhagyadeep <b.ghosh0330@gmail.com>
2024-04-10 19:46:38 +0530

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='bg-yellow-300 divide-top py-10'>
+      <div className='flex flex-col md:flex-row gap-5 justify-between container mx-auto py-5 r '>
+        {/* logo*/}
+        <div className='cursor-pointer'>
+            <img src="/Logo.svg" alt="logo" />
+        </div>
+        {/* quick links */}
+            <div className='uppercase space-y-3 '>
+                <h3 className='h3 '>Links</h3>
+                <p className='p text-[15px] cursor-pointer'>Something</p>
+                <p className='p text-[15px] cursor-pointer'>Something</p>
+                <p className='p text-[15px] cursor-pointer'>Something</p>
+                <p className='p text-[15px] cursor-pointer'>Something</p>
+            </div>
+            <div className='uppercase space-y-3'>
+                <h3 className='h3'>contact</h3>
+                <p className='p text-[15px] cursor-pointer'>mail</p>
+                <p className='p text-[15px] cursor-pointer'>number</p>
+            </div>
+        </div>
+    </section>
   )
 }
 
